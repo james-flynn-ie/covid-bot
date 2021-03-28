b5ed6d0dc8911bb84bff57e0dd9888e55df6e959
James Flynn <james-flynn-ie@users.noreply.github.com>
2021-03-28 12:28:02 +0100

    Added: Check for environment variables, outputs OSError if not set

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Retrieved on 14-March-2021 from: https://github.com/microsoft/BotBuilder-Samples/tree/main/samples/python/11.qnamaker
+# Adapted on 14-March-2021 from: https://github.com/microsoft/BotBuilder-Samples/tree/main/samples/python/11.qnamaker
 
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
@@ -11,6 +11,13 @@ import os
 
 class DefaultConfig:
     """ Bot Configuration """
+
+    # Check that mandatory env vars are set, raise error if not set.
+    MANDATORY_ENVIRONMENT_VARIABLES = ["MicrosoftAppId", "MicrosoftAppPassword", "QnAKnowledgebaseId", "QnAEndpointKey", "QnAEndpointHostName"]
+
+    for environmentVariable in MANDATORY_ENVIRONMENT_VARIABLES:
+        if environmentVariable not in os.environ:
+            raise EnvironmentError("Covid-Bot failed to start due to missing environment variable. Please set a value for \"{}\".".format(environmentVariable))
 
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
